037cf1da1a254aed5bde59513d4ecf3f47450a6f
Leone.Kim <leone@tourmktg.co.kr>
2021-08-14 20:19:07 +0900

    주석 추가

--- 2021-08-15/exercise_acmicpi_1956.py
+++ 2021-08-15/exercise_acmicpi_1956.py
@@ -1,20 +1,24 @@
 import sys
 
+#최대 거리는 10000
 vertexNum, edgeNum = map(int, input().rstrip().split(' '))
-roads = [[10001]*vertexNum for i in range(vertexNum)]
+edges = [[10001]*vertexNum for i in range(vertexNum)]
 
+#마을간 거리 입력
 for _ in range(edgeNum):
     start, end, cost = map(int, input().rstrip().split(' '))
-    roads[start - 1][end - 1] = cost
-    
+    edges[start - 1][end - 1] = cost
+
+#플로이드 와셜
 for k in range(vertexNum): 
     for i in range(vertexNum): 
         for j in range(vertexNum):
-            roads[i][j] = min(roads[i][j], roads[i][k] + roads[k][j])
+            edges[i][j] = min(edges[i][j], edges[i][k] + edges[k][j])
 
 minCycle = 10001;
+#사이클이 생긴 마을간 최소 거리 추출
 for i in range(vertexNum):
-    minCycle = min(roads[i][i], minCycle)
+    minCycle = min(edges[i][i], minCycle)
 
 if minCycle==10001:
     print(-1) 
